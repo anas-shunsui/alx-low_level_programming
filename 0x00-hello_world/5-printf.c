ea0bbcfc47aae4e1b1907215febc43198a8beffe
Anas <alas.anass1@gmail.com>
2023-06-15 16:07:18 -0700

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	fprintf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("%iwith proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
