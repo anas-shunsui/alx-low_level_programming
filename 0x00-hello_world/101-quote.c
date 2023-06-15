7fba13bdd52321ae086e6c9c29649b374ab57e47
Anas <alas.anass1@gmail.com>
2023-06-15 16:49:50 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/*
+ * main - entry point
+ *
+ * Return: always one (Success)
+ */
+int main(void)
+{
+	write(2."and that piece of art is useful\" - Dora Koroar, 2015-10-19\n");
+	return (1);
+}
