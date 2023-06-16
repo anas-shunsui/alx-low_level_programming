0400cdb3733e66f2ae6da9d7a397af7eefb7a8d3
Anas <alas.anass1@gmail.com>
2023-06-16 16:16:11 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int num;
+
+	fot (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
