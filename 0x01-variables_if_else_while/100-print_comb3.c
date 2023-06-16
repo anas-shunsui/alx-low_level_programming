b1996fe3295499d5b1dea3194ddca89807b8b4fc
Anas <alas.anass1@gmail.com>
2023-06-16 16:47:28 -0700

    Two diff combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints combinations of two digits
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int comb1, comb2;
+
+	for (comb1 = 48; comb1 <= 57; comb1++)
+	{
+		for (comb2 = 49; comb2 <= 57; comb2++)
+		{
+			if (comb2 < comb1)
+			{
+				putchar(comb1);
+				putchar(comb2);
+				if (comb1 != 56 || comb2 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
