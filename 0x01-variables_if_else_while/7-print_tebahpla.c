a44034867a5858e201b7487a6b9f9af8e6c03e6e
Anas <alas.anass1@gmail.com>
2023-06-16 16:24:00 -0700

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h<
+/**
+ * main - print alphabet in reverse
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph + 'z'; alph >= 'a'; alph--);
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
