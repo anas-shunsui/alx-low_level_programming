a47d104ccee23345f113fc239aba0b6940eab80b
Anas <alas.anass1@gmail.com>
2023-06-15 17:57:13 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 #include<unistd.h>
 /*
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline to standard error.
- * @main: anything
- * Return: Always 1
+ *main - Entry point
+ *Return: Always 1
  */
 int main(void)
 {
