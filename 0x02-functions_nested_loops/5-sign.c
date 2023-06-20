2a77b2b6cf812855ba736de7bc3937edc1ea4a62
Anas <alas.anass1@gmail.com>
2023-06-20 14:46:00 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - 1 prints + if n is greater than zero, 0 prints 0 if n is zero, -1 prints - if n is less than zero
+ *
+ * @c: input for function
+ *
+ * Return: 1 if +, 0 if 0, -1 if -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
