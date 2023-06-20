a7af1c4c88205c8263f601d87131b46a924050c3
Anas <alas.anass1@gmail.com>
2023-06-20 13:55:47 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabet - print alphabet in lowercase
+ *
+ */
+void alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
