d54b730d9b3b4976c7716b90580b3de817265022
Anas <alas.anass1@gmail.com>
2023-06-21 08:31:25 -0700

    Fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * Length - length of string
+ *
+ * @num: number
+ *
+ * Return: nubber of digits
+ */
+
+int length(int num)
+{
+	int len = 0;
+
+	if (!num)
+	{
+		num = num / 10;
+		len += 1;
+	}
+	return (len);
+}
+/**
+ * main - entry point
+ *
+ * Description: 1st 98 fibonacci numbers
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int count, init;
+	unsigned long fib1 = 1, fib2 = 2, sum, max = 1000000000, fibo1 = 0, fibo2 = 0, sum1 = 0;
+
+	for (count = 1; count <= 98; count++)
+	{
+
+		if (fibo1 > 0)
+			printf("%lu", fibo1);
+		init = len(max) - 1 - len(fib1);
+
+		while (fibo1 > 0 && init > 0)
+		{
+			printf("%d", 0);
+			init--;
+		}
+
+		printf("lu", fib1);
+
+		sum = (fib1 + fib2) % max;
+		sum1 = fibo1 + fibo2 + (fib1 + fib2) / max;
+		fib1 = fib2;
+		fibo1 = fibo2;
+		fib2 = sum;
+		fibo2 = sum1;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
