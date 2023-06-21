4608757d7113a934fb27794de60273ec43da92e4
Anas <alas.anass1@gmail.com>
2023-06-21 08:47:48 -0700

    Fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Length - length of string
+ * length - length of string
  *
  * @num: number
  *
@@ -30,29 +30,29 @@ int length(int num)
 int main(void)
 {
 	int count, init;
-	unsigned long fib1 = 1, fib2 = 2, sum, max = 1000000000, fibo1 = 0, fibo2 = 0, sum1 = 0;
+	unsigned long fi1 = 1, fi2 = 2, sum, mx = 1000000000, fib1 = 0, fib2 = 0, sum1 = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
 
-		if (fibo1 > 0)
-			printf("%lu", fibo1);
-		init = length(max) - 1 - length(fib1);
+		if (fib1 > 0)
+			printf("%lu", fib1);
+		init = length(mx) - 1 - length(fi1);
 
-		while (fibo1 > 0 && init > 0)
+		while (fib1 > 0 && init > 0)
 		{
 			printf("%d", 0);
 			init--;
 		}
 
-		printf("%lu", fib1);
+		printf("%lu", fi1);
 
-		sum = (fib1 + fib2) % max;
-		sum1 = fibo1 + fibo2 + (fib1 + fib2) / max;
+		sum = (fi1 + fi2) % mx;
+		sum1 = fib1 + fib2 + (fi1 + fi2) / mx;
+		fi1 = fi2;
 		fib1 = fib2;
-		fibo1 = fibo2;
-		fib2 = sum;
-		fibo2 = sum1;
+		fi2 = sum;
+		fib2 = sum1;
 
 		if (count != 98)
 			printf(", ");
