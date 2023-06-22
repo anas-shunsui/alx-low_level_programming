8e251cca2922340468713897e1defa5d7209a31a
Anas <alas.anass1@gmail.com>
2023-06-22 06:43:42 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ *
+ * Return: always zero
+ */
+
+void more_numbers(void)
+
+{
+	int num, row, count;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 40);
+		}
+		_putchar('\n');
+	}
+}
