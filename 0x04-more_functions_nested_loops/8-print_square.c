0b833806fa98a3bfa14411e83ecb82dba1c6a1d3
Anas <alas.anass1@gmail.com>
2023-06-22 08:18:30 -0700

    Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,11 @@ void print_square(int size)
 {
 	int row, column;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (row = 0; row < size; row++)
 	{
 		for (column = 0; column < size; column++)
