7780c7ec56b979f2423e3f9a527f1bb182f70e4c
Anas <alas.anass1@gmail.com>
2023-06-22 08:23:56 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,14 +11,20 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (postn = 0; postn < n; postn++)
 		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
-			_putchar(92);
+			for (space = 0; space < postn; space++)
+			{
+				if (space == postn)
+					_putchar('\\');
+				else if (space < postn)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
