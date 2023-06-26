ba5804197f51af961eaf98be8a0c95a3d33d5460
Anas <alas.anass1@gmail.com>
2023-06-26 13:33:33 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,13 +13,13 @@ void rev_string(char *s)
 	int a, b;
 	char c;
 
-	for (a = 0; s[a] != '\0'; ++a)
-		;
+	while (s[a] != '\0')
+		a++;
 
-	for (b = 0; b < 1 / 2; b++)
+	for (b = 0; b < a; b++)
 	{
-		c = s[b];
-		s[b] = s[a - 1 - b];
+		c = s[a];
+		s[a] = s[a - 1 - b];
 		s[a - 1 - b] = c;
 	}
 }
