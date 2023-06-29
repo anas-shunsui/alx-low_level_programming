aaa5b2a95bbeeea55d14f4c9138643200fae61e6
Anas <alas.anass1@gmail.com>
2023-06-29 16:49:51 -0700

    Changes lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Function that changes all lowercase letters to uppercase
+ *
+ * @str: string
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
