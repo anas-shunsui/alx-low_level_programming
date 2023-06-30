68f073a5fbefa7d5ff693e39b27a9778ef159e7b
Anas <alas.anass1@gmail.com>
2023-06-29 18:40:37 -0700

    lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,22 +3,22 @@
 /**
  * string_toupper - changes lowercase to uppercase
  *
- * @str: pointer
+ * @a: pointer
  *
  * Return: str
  */
 
-char *string_toupper(char *str)
+char *string_toupper(char *a)
 {
 	int i;
 	
 	i = 0;
 
-	while (str[i] != '\0')
+	while (a[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-			str[i] = str[i] - 32;
+		if (a[i] >= 97 && a[i] <= 122)
+			a[i] = a[i] - 32;
 		i++;
 	}
-	return (str);
+	return (a);
 }
