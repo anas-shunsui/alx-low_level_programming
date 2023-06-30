6da4c5036e2282042ac10b268d12d89662d11dde
Anas <alas.anass1@gmail.com>
2023-06-29 17:09:55 -0700

    Changes lowercase letters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,12 +12,12 @@ char *string_toupper(char *str)
 	int i;
 	
 	i = 0;
-	while (n[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
 		i++;
 	}
 
-	return (n);
+	return (str);
 }
