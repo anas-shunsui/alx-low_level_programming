fa535a6947f30478f082bacb56ef1a31a430f11b
Anas <alas.anass1@gmail.com>
2023-06-29 19:34:35 -0700

    Capitalizes all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,20 @@ char *cap_string(char *a)
 		while (!(a[ind] >= 'a' && a[ind] <= 'z'))
 			ind++;
 
-		if (a[ind - 1] == ' ' || a[ind - 1] == '\t' || a[ind - 1] == '\n' || a[ind - 1] == ',' || a[ind - 1] == ';' || a[ind - 1] == '.' || a[ind - 1] == '!' || a[ind - 1] == '?' || a[ind - 1] == '"' || a[ind - 1] == '(' || a[ind - 1] == ')' || a[ind - 1] == '{' || a[ind - 1] == '}' || ind == 0)
+		if (a[ind - 1] == ' ' ||
+		a[ind - 1] == '\t' ||
+		a[ind - 1] == '\n' ||
+		a[ind - 1] == ',' ||
+		a[ind - 1] == ';' ||
+		a[ind - 1] == '.' ||
+		a[ind - 1] == '!' ||
+		a[ind - 1] == '?' ||
+		a[ind - 1] == '"' ||
+		a[ind - 1] == '(' ||
+		a[ind - 1] == ')' ||
+		a[ind - 1] == '{' ||
+		a[ind - 1] == '}' ||
+		ind == 0)
 			a[ind] -= 32;
 		ind++;
 	}
