ab0486e15848229d0cc787cb639e051492809da6
anas-shunsui <alas.anass1@gmail.com>
2023-07-03 20:57:10 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+EVEN More pointers arrays and strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+char *_mamset(char *s, char b, unsigned int n);
+
+
+#endif
