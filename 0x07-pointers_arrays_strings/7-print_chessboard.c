3b512af137baa99871dc37bfab35f39b7dc210d4
anas-shunsui <alas.anass1@gmail.com>
2023-07-04 01:43:22 +0100

    Prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints he chessboard
+ * @a: array
+ *
+ * Return: always zero
+ */
+
+void print_chessboard(char [*a][8])
+{
+	int y, z;
+
+	for (y = 0; y < 8; y++)
+	{
+		for (z = 0; z < 8; z++)
+			_putchar(a[y][z]);
+		_putchar('\n');
+	}
+}
