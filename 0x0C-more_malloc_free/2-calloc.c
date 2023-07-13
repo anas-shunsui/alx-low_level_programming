6c81443d61e8ee2f6c1f021b24a5e94b09680d43
anas-shunsui <alas.anass1@gmail.com>
2023-07-13 21:43:31 +0100

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_func - memory with constant byte
+ * @str: pointer
+ * @c: constant
+ * @b: byte
+ *
+ * Retrun: str
+ */
+
+char *_func(char *str, char c, unsigned int b)
+{
+	char *ptr = str;
+
+	while (b--)
+		*str++ = c;
+
+	return (ptr);
+}
+
+/**
+ * *_calloc - allocates memory for an array using malloc
+ * @nmemb: array
+ * @size: size
+ *
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *a;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	a = malloc(sizeof(int) * nmemb);
+
+	if (a == 0)
+		return (NULL);
+
+	_func(a, 0, sizeof(int) * nmemb);
+
+	return (a);
+}
