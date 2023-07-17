8eac091b6d9ebf1c9c3129023ae37196fcc53c60
anas-shunsui <alas.anass1@gmail.com>
2023-07-17 21:10:15 +0100

    Source file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints name of file it was compiled from
  *
- * Return : Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
