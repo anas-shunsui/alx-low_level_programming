e1c007b1f2a46461e5c3d40e1516fd93976bbc07
anas-shunsui <alas.anass1@gmain.com>
2023-08-04 20:02:59 +0100

    libmy done

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _atoi - convetrs string to integer
+ *
+ * @s: string
+ *
+ * Return: int converted from string
+ */
+
+int _atoi(char *s)
+{
+	int i, a, b, len, digit, c;
+
+	i = 0;
+	a = 0;
+	b = 0;
+	len = 0;
+	digit = 0;
+	c = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && c == 0)
+	{
+		if (s[i] == '-')
+			++a;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (a % 2)
+				digit = -digit;
+			b = b * 10 + digit;
+			c = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			c = 0;
+		}
+		i++;
+	}
+
+	if (c == 0)
+		return (0);
+
+	return (b);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: bytes
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != s[a]; b++)
+		{
+			if (accept[b] == '\0')
+				return (a);
+		}
+	}
+	return (a);
+}
