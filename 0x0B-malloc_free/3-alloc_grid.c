2ffbb5d813576f7ea80bf5267f78322772810955
anas-shunsui <alas.anass1@gmain.com>
2023-08-08 00:36:31 +0100

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: if width or height is 0 or negative, return NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **i;
+	int a, b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	i = malloc(sizeof(int *) * height);
+
+	if (i == NULL)
+		return (NULL);
+	
+	for (a = 0; a < height; a++)
+	{
+		i[a] = malloc(sizeof(int) * width);
+
+		if (i[a] == NULL)
+		{
+			for (; a >= 0; a--)
+				free(i[a]);
+
+			free(i);
+			return (NULL);
+		}
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+			mee[a][b] = 0;
+	}
+
+	return (i);
+}
