daf731fb7d90637586d87cd0331a25adb9bb098e
anas-shunsui <alas.anass1@gmain.com>
2023-08-10 19:58:30 +0100

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_calloc - allocates memory for an array, using malloc
+ * @nmemb: array
+ * @size: size
+ *
+ * Return: NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *result;
+	unsigned int n;
+
+	if (nmemb == NULL || size == NULL)
+		return (NULL);
+
+	result = malloc(nmemb * size);
+	if (result == 0)
+		return (NULL);
+
+	n = 0;
+	while (n < nmemb * size)
+	{
+		result[n] = 0;
+		n++;
+	}
+
+	return (result);
+}
