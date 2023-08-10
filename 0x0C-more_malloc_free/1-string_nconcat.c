892d241e1649ad88faee17f04af5dd75ebf8b11d
anas-shunsui <alas.anass1@gmain.com>
2023-08-10 18:31:27 +0100

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (s1_len = 0; s1[s1_len] != '\0', s1_len++)
-		printf("%d\n", s1_len);
+		;
 	for (s2_len = 0; s2[s2_len] != '\0', s2_len++)
-		printf("%d\n", s2_len);
+		;
 
 	result = malloc(s1_len + n + 1);
 	if (result == NULL)
