ebbfc359c54b1e09fcfe80cb4e44e9456629e34f
anas-shunsui <alas.anass1@gmain.com>
2023-08-14 23:19:54 +0100

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+* main - entry point
+* Description: prints the name of the file it was compiled from
+*
+* Return: always zero
+*/
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
