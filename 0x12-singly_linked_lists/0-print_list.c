e9b67c23738172896b4358abb9f3100cc49eeba4
anas-shunsui <alas.anass1@gmail.com>
2023-09-21 21:18:17 +0100

    0.Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - length of string
- * @st: string
- *
- * Return: length of str
-
-
-int _strlen(char *st)
-{
-	int a = 0;
-
-	if (!a)
-		return (0);
-	while (*st++)
-		a++;
-	return (a);
-}
-**/
-/**
  * print_list - prints all elements of a list
  * @h: node
  *
