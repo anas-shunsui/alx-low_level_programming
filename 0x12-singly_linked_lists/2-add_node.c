d2647a661c71e6d28b62c2c4de2a1dc60a342e1c
anas-shunsui <alas.anass1@gmail.com>
2023-09-21 21:50:01 +0100

    2. Add NOde

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,18 +12,18 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_h = malloc(sizeof(list_t));
 
-	if (head = NULL || new_h = NULL)
+	if (str == NULL)
 		return (NULL);
-	if (str != NULL)
-	{
-		new_h->str = strdup(str);
-		if (!new_h->str)
-		{
-			free(new_h);
-			return (NULL);
-		}
-	}
 
+	if (new_h == NULL)
+		return (NULL);
+
+	new_h->str = strdup(str);
+	if (new_h->str == NULL)
+	{
+		free(new_h);
+		return (NULL);
+	}
 	new_h->next = *head;
 	*head = new_h;
 
