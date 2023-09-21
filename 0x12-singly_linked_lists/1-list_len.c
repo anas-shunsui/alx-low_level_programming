cdfa6823e5b2abfeff4ae67947524c642de110d1
anas-shunsui <alas.anass1@gmail.com>
2023-09-21 21:24:47 +0100

    1.List Length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len -  returns the number of elements in a linked list
+ * @h: node
+ *
+ * Return: len of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
