7ad94091a90e4a0de0a49a76a1978a00a61c9cbf
anas-shunsui <alas.anass1@gmail.com>
2023-09-26 21:41:54 +0100

    9.Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_n->n = n;
 	new_n->next = NULL;
-	if (!idx)
+	if (idx)
 	{
 		new_n->next = *head;
 		*head = new_n;
