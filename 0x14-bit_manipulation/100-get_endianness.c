7fe50d45e454393654eb353f48b687f4d72edf76
anas-shunsui <alas.anass1@gmail.com>
2023-10-30 21:52:18 +0100

    6. Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * 
+ * Return: 0 if big endian, 1 if little endian
+*/
+
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *b = (char *)&n;
+	
+	if (*b == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
